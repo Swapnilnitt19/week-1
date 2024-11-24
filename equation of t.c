b63a4c69ca1768a28429b6f747272718426a8dfd
Swapnilnitt19 <swapnil.nitt1630@gmail.com>
2024-11-24 17:47:46 +0530

    Add files via upload

---
+++ equation of t.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float a, b;
+    int result_int;
+
+    printf("Enter two floating-point numbers: ");
+    scanf("%f %f", &a, &b);
+
+    float = a * b;
+
+
+    printf("Result as a float: %.2f\n", );
+    printf("Result as an integer: %d\n", result_int);
+
+    return 0;
+}
+
---
+++ miscellaneous equation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{ 
+int T,a,b,c;
+printf("enter the value of a : ");
+scanf("%d",&a);
+printf("enter the value of b : ");
+scanf("%d",&b);
+printf("enter the value of c : ");
+scanf("%d",&c);
+ T=2*a+sqrt(b)+9*c;
+ printf("the value of T is %d",T);
+ return 0;
+
+}
+
+
