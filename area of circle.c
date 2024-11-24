a77b34f0c3d41b8b1a0f82cca31553e7a8ec614d
Swapnilnitt19 <swapnil.nitt1630@gmail.com>
2024-11-24 17:34:21 +0530

    Add files via upload

---
+++ area of circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int r;
+	float pi=3.14,area;
+	printf("\n enter the radius of the circle=");
+	scanf("%d",&r);
+	area=pi*r*r;
+	printf("\n area of circle=%f",area);
+	return 0;
+}
+
